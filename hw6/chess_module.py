df7097a67f40d21d42fcac8cb134e953171e33c4
Anna <tka4evaanna2002@yandex.ru>
2023-09-26 18:10:04 +0300

    task added

--- hw6/chess_module.py
+++ hw6/chess_module.py
@@ -1,20 +1,22 @@
+from random import randint
 import random
-# Добавьте в пакет, созданный на семинаре шахматный модуль.
-#  Внутри него напишите код, решающий задачу о 
-# 8 ферзях. Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга.
-
-# Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга.
- 
-# Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей. 
-# Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
 
 # Напишите функцию в шахматный модуль. Используйте генератор случайных чисел для случайной
 # расстановки ферзей в задаче выше. Проверяйте различный случайные варианты и выведите 4 успешных расстановки.
 a = [[1,2],[2,4],[2,4],[3,6],[7,8],[3,6],[2,3],[3,3]]
+
 def check_queens(positions: list[list]):
-    j = random.choice(positions)
-    for i in range(len(positions)-1):       
-            if positions[i][0] == positions[j][0] or positions[i][1] == positions[j][1]:
-                print(positions[i][0], positions[i+1][0], "/", positions[i][1], positions[i+1][1])
-                continue
+    for i in range(len(positions)):
+        for j in range(1, len(positions)):      
+            if (positions[i][0] == positions[j][0] or \
+            positions[i][1] == positions[j][1] or \
+            abs(positions[i][0] - positions[j][0]) == abs(positions[i][1] - positions[j][1])):
+                return False      
+    return True
+
 print(check_queens(a))
+
+def gen_pos():
+    positions = [[random.randint(1,8), random.randint(1,8)] for i in range(8)]
+    return positions
+print(gen_pos())
